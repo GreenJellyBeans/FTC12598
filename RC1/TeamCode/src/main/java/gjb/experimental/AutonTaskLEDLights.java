48adf640d318288f11ea44c818b2f1829c637127
Joseph Joy <josephmjoy@hotmail.com>
2017-09-26 21:36:58 -0700

    Skeleton refactored LED lights code
    These are just skeleton classes - AutonTaskLEDLights and SubSysLights

---
+++ RC1/TeamCode/src/main/java/gjb/experimental/AutonTaskLEDLights.java
@@ -0,0 +1,39 @@
+package gjb.experimental;
+
+import gjb.interfaces.TaskInterface;
+
+/**
+ * Created by josephj on 9/24/2017.
+ */
+
+public class AutonTaskLEDLights implements TaskInterface {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public double getRuntime() {
+        return 0;
+    }
+}
---
+++ RC1/TeamCode/src/main/java/gjb/experimental/SubSysLights.java
@@ -0,0 +1,19 @@
+package gjb.experimental;
+
+import gjb.interfaces.SubSystemInterface;
+
+/**
+ * Created by josephj on 9/24/2017.
+ */
+
+public class SubSysLights implements SubSystemInterface {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void deinit() {
+
+    }
+}
