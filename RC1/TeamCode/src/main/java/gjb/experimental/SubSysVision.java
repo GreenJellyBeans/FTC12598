82c5a05c23115cf45d61fa8c3179494a950d0234
Joseph Joy <josephmjoy@hotmail.com>
2017-10-11 20:30:31 -0700

    Vuforia support continued
    Forgot to checkin this class with previous checkin.

--- RC1/TeamCode/src/main/java/gjb/experimental/SubSysVision.java
+++ RC1/TeamCode/src/main/java/gjb/experimental/SubSysVision.java
@@ -3,6 +3,12 @@
  */
 package gjb.experimental;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
 import java.util.prefs.BackingStoreException;
 
 import gjb.interfaces.LoggingInterface;
@@ -53,7 +59,18 @@ public class SubSysVision implements SubSystemInterface {
     final String THIS_COMPONENT = "SS_VISION"; // // Replace EMPTY by short word identifying task
     final public RuntimeSupportInterface rt;
     final public LoggingInterface log;
+
     // Place additional instance variables here - like hardware access objects
+    final Config cfg;
+    private boolean inited = false;
+    private boolean tracking = false;
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    VuforiaLocalizer vuforia;
+    VuforiaTrackables trackableList;
+    VuforiaTrackable trackable;
 
 
     // Modify this constructor to add any additional initialization parameters - see
@@ -61,7 +78,28 @@ public class SubSysVision implements SubSystemInterface {
     public SubSysVision(RuntimeSupportInterface rt, Config cfg) {
         this.rt = rt;
         this.log = rt.getRootLog().newLogger(THIS_COMPONENT); // Create a child log.
+        this.cfg = cfg;
     }
+
+    public synchronized void  startTracking() {
+        if (!tracking) {
+            trackableList.activate();
+            tracking = true;
+        } else {
+            log.loggedAssert(false, "Attempt to start tracking when tracking has already started");
+        }
+    }
+
+
+    public synchronized void stopTracking() {
+        if (tracking) {
+            trackableList.deactivate();
+            tracking = true;
+        } else {
+            log.loggedAssert(false, "Attempt to stop tracking when tracking has already stopped");
+        }
+    }
+
 
 
     /********* START OF SUBSYSTEM INTERFACE METHODS ***************/
@@ -70,6 +108,29 @@ public class SubSysVision implements SubSystemInterface {
     public void init() {
         this.log.pri1(LoggingInterface.INIT_START, "");
         // Any subsystem initialization code goes here.
+        VuforiaLocalizer.Parameters parameters;
+        if (cfg.enableDisplay) {
+            int cameraMonitorViewId = rt.getIdentifierFromPackage("cameraMonitorViewId", "id");
+            parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        } else {
+            parameters = new VuforiaLocalizer.Parameters();
+        }
+        parameters.cameraDirection = (cfg.cameraDirection == CameraDirection.FRONT)
+                ? VuforiaLocalizer.CameraDirection.FRONT
+                : VuforiaLocalizer.CameraDirection.BACK;
+                                    ;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        /**
+         * Load the data set containing the VuMarks for Relic Recovery. There's only one trackable
+         * in this data set: all three of the VuMarks in the game were created from this one template,
+         * but differ in their instance id information.
+         * @see VuMarkInstanceId
+         */
+        trackableList = this.vuforia.loadTrackablesFromAsset(cfg.trackableAssetName);
+        trackable = trackableList.get(0);
+        trackable.setName(cfg.trackableAssetName); // can help in debugging; otherwise not necessary
+
         this.log.pri1(LoggingInterface.INIT_END, "");
     }
 
@@ -79,6 +140,14 @@ public class SubSysVision implements SubSystemInterface {
         this.log.pri1(LoggingInterface.DEINIT_START, "");
         // Place any shutdown/deinitialization code here  - this is called ONCE
         // after tasks & OpModes have stopped.
+        if (tracking) {
+            stopTracking();
+        }
+
+        trackable = null;
+        trackableList = null;
+        vuforia=null;
+
         this.log.pri1(LoggingInterface.DEINIT_END, "");
 
     }
