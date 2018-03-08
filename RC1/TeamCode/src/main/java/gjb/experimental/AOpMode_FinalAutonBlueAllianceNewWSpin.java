51b2fd909a6213afcdd9da46c79780c5aada7d8b
Joseph Joy <josephmjoy@hotmail.com>
2018-03-07 21:38:40 -0800

    keya aparna joseph
    created two new copies of alliance programs with different jewel-knocking mechanisms

---
+++ RC1/TeamCode/src/main/java/gjb/experimental/AOpMode_FinalAutonBlueAllianceNewWSpin.java
@@ -0,0 +1,41 @@
+
+package gjb.experimental;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import gjb.interfaces.RuntimeSupportInterface;
+import gjb.utils.AndroidRuntimeSupport;
+
+/**
+ * New version of auton code - with most of the logic moved to helper class
+ * AutonWizard
+  */
+
+@Autonomous(name="AOpMode_FinalAuton_BLUE_ALLIANCE_NEW", group="Pushbot")
+//@Disabled
+public class AOpMode_FinalAutonBlueAllianceNewWSpin extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    final String THIS_COMPONENT = "AOpMode_SimpleAuton";
+    private final RuntimeSupportInterface rt = new AndroidRuntimeSupport(this);
+
+
+    AutonWizard apu = null;
+
+    @Override
+    public void runOpMode() {
+        double timeoutS;
+
+        apu = new AutonWizard(rt) ;
+        apu.init();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Work the wand, detect jewel color and move forward/backward to dislodge
+        // the jewel - this is code specific to the BLUE alliance
+        apu.getJewelBlueAlliance();
+    }
+
+}
