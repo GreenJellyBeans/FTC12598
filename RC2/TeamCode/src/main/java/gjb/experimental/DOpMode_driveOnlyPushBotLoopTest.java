24b941e05d88867b0e7f850b2319d7cc9516f550
gjb-dev1 <32212126+gjb-dev1@users.noreply.github.com>
2018-09-02 13:45:08 -0700

    Loop Counter Test
    Checking how many times loop is called on driver station.
    Changes made by: Aparna

---
+++ RC2/TeamCode/src/main/java/gjb/experimental/DOpMode_driveOnlyPushBotLoopTest.java
@@ -0,0 +1,73 @@
+/**
+ * Created by github.com/josephmjoy (mentor for FTC#12598 & FRC#1899) on 9/30/2017.
+ */
+package gjb.experimental;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import gjb.interfaces.LoggingInterface;
+import gjb.interfaces.RuntimeSupportInterface;
+import gjb.utils.AndroidRuntimeSupport;
+
+@TeleOp(name="DOpMode_driveOnlyPushBot-v1", group="Pushbot")
+//@Disabled
+/*
+ *  This Driver Controlled OpMode does controls the wheels of the pushbot.
+ */
+public class DOpMode_driveOnlyPushBotLoopTest extends OpMode{
+    final String THIS_COMPONENT = "DOM_driveOnlyPushBot";
+    private final RuntimeSupportInterface rt = new AndroidRuntimeSupport(this);
+    int loopCounter = 0;
+
+    // These are initialized during init()
+    private  SubSysSimpleTwoMotorDrive drive;
+    private ITask_simpleDrive driveTask;
+    private LoggingInterface log;
+
+    /*************** START OF OPMODE INTERFACE METHODS **********************/
+
+    @Override
+    public void init() {
+        log = rt.startLogging(DOpMode_driveOnlyPushBotLoopTest.class.toString());
+        log.pri1(LoggingInterface.INIT_START, THIS_COMPONENT);
+
+        SubSysSimpleTwoMotorDrive.Config driveConfig = new SubSysSimpleTwoMotorDrive.Config();
+        driveConfig.leftMotorName("left_drive");
+        driveConfig.rightMotorName("right_drive");
+        drive = new SubSysSimpleTwoMotorDrive(rt, driveConfig);
+        driveTask = new ITask_simpleDrive(rt, drive);
+
+        // Initialize the subsystem and associated task
+        drive.init();
+        driveTask.init();
+
+        log.pri1(LoggingInterface.INIT_END, THIS_COMPONENT);
+    }
+
+
+    @Override
+    public void init_loop() {
+        driveTask.init_loop();
+    }
+
+    @Override
+    public void start() {
+        driveTask.start();
+    }
+
+    @Override
+    public void loop() {
+        rt.telemetry().addData("loop counter",this.loopCounter );
+        driveTask.loop();
+        loopCounter++;
+    }
+
+    @Override
+    public void stop() {
+        driveTask.stop();
+        rt.stopLogging();
+    }
+
+    /*************** END OF OPMODE INTERFACE METHODS ************************/
+}
