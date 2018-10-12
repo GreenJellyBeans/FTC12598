0b251da1faa075ff79dbbc56b1a86082d1eff8a4
gjb-dev1 <32212126+gjb-dev1@users.noreply.github.com>
2018-10-12 12:09:52 -0700

    Creating a Position Information Class - Aparna
    We created a Position Information class to get our bearing and position, and the bearing and location we want to go to, and some other empty methods are in here too.

---
+++ RC2/TeamCode/src/main/java/gjb/experimental/PositionInformation.java
@@ -0,0 +1,76 @@
+/**
+ * Created by github.com/josephmjoy (mentor for FTC#12598 & FRC#1899) on 9/30/2017.
+ */
+package gjb.experimental;
+
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import gjb.interfaces.LoggingInterface;
+import gjb.interfaces.RuntimeSupportInterface;
+import gjb.utils.AndroidRuntimeSupport;
+
+
+public class PositionInformation {
+    final String THIS_COMPONENT = "PI";
+    // These are initialized during init()
+
+    SubSysEmpty empty;
+    ITask_Empty task;
+    private LoggingInterface log;
+
+
+    // Place additional instance variable for this OpMode here
+
+
+    /*************** START OF OPMODE INTERFACE METHODS **********************/
+
+
+    public void init() {
+
+
+
+
+
+    }
+
+    public void setCurrentPosition(){
+
+    }
+
+    public void setTarget(){
+
+    }
+
+    public void setCurrentBearing(){
+
+    }
+
+    public void distanceToTarget(){
+
+    }
+
+    public void bearingToTarget(){
+
+    }
+
+
+    void initVuforia() {
+
+    }
+
+
+
+
+
+
+
+    // Place additional helper methods here.
+
+
+/*************** END OF OPMODE INTERFACE METHODS ************************/
+
+}
