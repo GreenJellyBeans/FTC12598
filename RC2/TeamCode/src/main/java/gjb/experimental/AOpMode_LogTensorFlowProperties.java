bea1c1a10e1ce34d45f0d9566e53a525d049a000
gjb-dev1 <32212126+gjb-dev1@users.noreply.github.com>
2018-12-14 18:28:05 -0800

    Moved AOpMode_LogTensorFlowProperties back up to experimental
    We were seeing strange complaints of phantom duplicate op modes with the sub-project "Investigations"  that we didn't want to spend time resolving so keeping all opmodes in a single directory for now.

--- RC2/TeamCode/src/main/java/gjb/experimental/AOpMode_LogTensorFlowProperties.java
+++ RC2/TeamCode/src/main/java/gjb/experimental/AOpMode_LogTensorFlowProperties.java
@@ -3,7 +3,7 @@
  * to log all Recognition object properties each time the A button is pressed.
  */
 
-package gjb.experimental.Investigations;
+package gjb.experimental;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -19,7 +19,7 @@ import java.util.List;
 
 @TeleOp(name = "A_OpMode_LogTensorFlowProperties", group = "Concept")
 //@Disabled
-public class AOpMode_LogTensorFlowRecognitionProperties extends LinearOpMode {
+public class AOpMode_LogTensorFlowProperties extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
     private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
     private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
