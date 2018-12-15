22ecbd83ba1879a62b01335cc796211a4a1d0ff7
gjb-dev1 <32212126+gjb-dev1@users.noreply.github.com>
2018-12-14 21:34:51 -0800

    Fixing a diagnostics code error - keya

--- RC2/TeamCode/src/main/java/gjb/experimental/DOpMode_LEDTestBlinkin.java
+++ RC2/TeamCode/src/main/java/gjb/experimental/DOpMode_LEDTestBlinkin.java
@@ -20,7 +20,7 @@ import gjb.utils.Logger;
 public class DOpMode_LEDTestBlinkin extends OpMode{
     final String THIS_COMPONENT = "DOM_driveOnlyPushBot";
     private final RuntimeSupportInterface rt = new AndroidRuntimeSupport(this);
-
+    public AutonRoverRuckusWizard apu = new AutonRoverRuckusWizard(rt);
     // These are initialized during init()
     private  SubSysDiagnostics diagnostics;
 
@@ -29,7 +29,7 @@ public class DOpMode_LEDTestBlinkin extends OpMode{
     @Override
 
     public void init() {
-        diagnostics  = new SubSysDiagnostics(rt);
+        diagnostics  = new SubSysDiagnostics(rt, apu);
         // Initialize the subsystem and associated task
         diagnostics.init();
     }
