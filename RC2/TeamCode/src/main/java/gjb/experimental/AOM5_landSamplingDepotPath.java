98a427609399ab2b54a58afd023ecc9e4cd82e3a
gjb-dev1 <32212126+gjb-dev1@users.noreply.github.com>
2018-12-23 15:55:48 -0800

    Fixing OpMode names - Aparna

--- RC2/TeamCode/src/main/java/gjb/experimental/AOM5_landSamplingDepotPath.java
+++ RC2/TeamCode/src/main/java/gjb/experimental/AOM5_landSamplingDepotPath.java
@@ -12,9 +12,9 @@ import gjb.utils.AndroidRuntimeSupport;
  * AutonWizard
   */
 
-@Autonomous(name="AOM4_landSamplingCraterPath", group="Pushbot")
+@Autonomous(name="AOM2_landSamplingDepotPath", group="Pushbot")
 //@Disabled
-public class AOM2_landSamplingCraterPath extends LinearOpMode {
+public class AOM5_landSamplingDepotPath extends LinearOpMode {
 
     /* Declare OpMode members. */
 
@@ -35,7 +35,7 @@ public class AOM2_landSamplingCraterPath extends LinearOpMode {
 
         // Work the wand, detect jewel color and move forward/backward to dislodge
         // the jewel - this is code specific to the BLUE alliance
-        apu.landSamplingCraterPath();
+        apu.landSamplingStraightDepotOtherCraterTestPath();
     }
 
 }
