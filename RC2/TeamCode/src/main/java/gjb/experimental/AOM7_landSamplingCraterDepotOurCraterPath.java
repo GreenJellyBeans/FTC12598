6bfeb1d6300c9a901b460ad1b7ffdce413ee4188
gjb-dev1 <32212126+gjb-dev1@users.noreply.github.com>
2018-12-23 16:03:55 -0800

    Making OpModes for all wanted paths - Aparna

---
+++ RC2/TeamCode/src/main/java/gjb/experimental/AOM7_landSamplingCraterDepotOurCraterPath.java
@@ -0,0 +1,41 @@
+
+package gjb.experimental;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import gjb.interfaces.RuntimeSupportInterface;
+import gjb.utils.AndroidRuntimeSupport;
+
+/**
+ * New version of auton code - with most of the logic moved to helper class
+ * AutonWizard
+  */
+
+@Autonomous(name="AOM2_landSamplingCraterDepotOurCraterPath", group="Pushbot")
+//@Disabled
+public class AOM7_landSamplingCraterDepotOurCraterPath extends LinearOpMode {
+
+    /* Declare OpMode members. */
+
+    private final RuntimeSupportInterface rt = new AndroidRuntimeSupport(this);
+
+
+    AutonRoverRuckusWizard apu = null;
+
+    @Override
+    public void runOpMode() {
+        double timeoutS;
+
+        apu = new AutonRoverRuckusWizard(rt) ;
+        apu.init();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Work the wand, detect jewel color and move forward/backward to dislodge
+        // the jewel - this is code specific to the BLUE alliance
+        apu.landSamplingCraterDepotOurCraterPath();
+    }
+
+}
