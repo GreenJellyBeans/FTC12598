2da5e755060713e512f36bd79861aa2bb79574c9
gjb-dev1 <32212126+gjb-dev1@users.noreply.github.com>
2019-01-19 15:55:02 -0800

    Testing whether robot strafes in a straight line - Aparna

---
+++ RC2/TeamCode/src/main/java/gjb/experimental/StrafeTest.java
@@ -0,0 +1,40 @@
+
+package gjb.experimental;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import gjb.interfaces.RuntimeSupportInterface;
+import gjb.utils.AndroidRuntimeSupport;
+
+/**
+ * New version of auton code - with most of the logic moved to helper class
+ * AutonWizard
+  */
+
+@Autonomous(name="Strafe Test", group="Pushbot")
+//@Disabled
+public class StrafeTest extends LinearOpMode {
+
+    /* Declare OpMode members. */
+
+    private final RuntimeSupportInterface rt = new AndroidRuntimeSupport(this);
+
+
+    AutonRoverRuckusWizard apu = null;
+
+    @Override
+    public void runOpMode() {
+        double timeoutS;
+
+        apu = new AutonRoverRuckusWizard(rt) ;
+        apu.init();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+
+        apu.strafeTest();
+    }
+
+}
