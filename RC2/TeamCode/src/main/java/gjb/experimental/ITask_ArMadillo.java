2c48f28b9d34e26cad9c83ffa0e1bfb7d5eb6dc9
gjb-dev1 <32212126+gjb-dev1@users.noreply.github.com>
2019-01-27 17:21:15 -0800

    Making an armadillo itask - Aparna

---
+++ RC2/TeamCode/src/main/java/gjb/experimental/ITask_ArMadillo.java
@@ -0,0 +1,116 @@
+/**
+ * Class created by Aparna, Keya, and Mira
+ * FTC #12598 on 10/8/17
+ */
+package gjb.experimental;
+
+import gjb.interfaces.LoggingInterface;
+import gjb.interfaces.RuntimeSupportInterface;
+import gjb.interfaces.TaskInterface;
+
+
+public class ITask_ArMadillo implements TaskInterface {
+
+    final String THIS_COMPONENT = "T_EMPTY"; // Replace EMPTY by short word identifying task
+
+
+    final RuntimeSupportInterface rt; // Runtime support
+    final LoggingInterface log; // Logger
+    final double DILLO_FWD = 0.2; //change later
+    final double DILLO_BKWD = -0.2; //change later
+
+    // Place additional instance variables here - like sub system objects..
+    SubSysIntake intakeS;
+
+
+    // Modify this constructor to add any additional initialization parameters - see
+    // other tasks for examples.
+    public ITask_ArMadillo(RuntimeSupportInterface rt, SubSysIntake intake) {
+        this.rt = rt;
+        this.intakeS  = intake;
+        this.log = rt.getRootLog().newLogger(THIS_COMPONENT);
+    }
+
+
+    /****** START OF TASK INTERFACE METHODS *********************/
+    @Override
+    public void init() {
+        this.log.pri1(LoggingInterface.INIT_START, "");
+        // Any task initialization code goes here.
+        this.log.pri1(LoggingInterface.INIT_END, "");
+    }
+
+
+    @Override
+    public void init_loop() {
+        // Code that must execute WHILE waiting for START goes here.
+    }
+
+
+    @Override
+    public void start() {
+        rt.resetStartTime();
+        // Any code to execute ONCE just when the op mode is started goes here.
+    }
+
+
+
+    @Override
+    public void loop() {
+
+        rt.telemetry().addData("joystick_value", rt.gamepad2().right_stick_y());
+        //we inverted the check since we are using magnetic limit switches
+        double power = 0;
+        if (!intakeS.limit_switch_forward.getState()) {
+            //Can't go forward anymore
+            rt.telemetry().addData("limit_switch_forward", intakeS.limit_switch_forward.getState());
+            power = 0;
+            if (-rt.gamepad2().left_stick_y() > 0.2) { //negating joystick value as a test, and it works
+                power = DILLO_BKWD;
+            }
+        } else if (-rt.gamepad2().left_stick_y() < -0.2) { //change value later, testing needs to be done, negating joystick valur as a test
+            rt.telemetry().addData("limit_switch_forward", "LOW");
+            power = DILLO_FWD;
+        }
+
+
+
+
+
+
+        //we inverted the check since we are using magnetic limit switches
+        if (!intakeS.limit_switch_backward.getState()) {
+            //Can't go backward anymore
+            rt.telemetry().addData("limit_switch_backward", "HIGH");
+            power = 0;
+            if (-rt.gamepad2().left_stick_y() < -0.2) {//change value later, testing needs to be done //negating joystick valur as a test
+                power = DILLO_FWD;
+            }
+        } else if (-rt.gamepad2().left_stick_y() > 0.2){ //change value later, testing needs to be done //negating joystick valur as a test
+            rt.telemetry().addData("limit_switch_backward", "LOW");
+
+            power = DILLO_BKWD;
+        }
+        rt.telemetry().addData("motor power", power);
+        intakeS.ArMadillo.setPower(power);
+
+
+
+        // Send telemetry message to signify robot running;
+       // rt.telemetry().addData("claw",  "Offset = %.2f", clawOffset);
+    }
+
+    @Override
+    public void stop() {
+        this.log.pri1("STOP", "");
+        // Place any shutdown/deinitialization code here  - this is called ONCE
+        // when the task is to be stopped.
+    }
+
+
+    /************* END OF TASK INTERFACE METHODS ****************/
+
+
+    // Place additional helper methods here.
+
+}
