4ec4afff710b9a79438d291e0768bed557110101
gjb-dev1 <32212126+gjb-dev1@users.noreply.github.com>
2019-02-21 01:28:14 -0800

    Auton assist code adding/revising *UNTESTED* - mira, keya

--- RC2/TeamCode/src/main/java/gjb/experimental/ITask_TwoPartArmMC.java
+++ RC2/TeamCode/src/main/java/gjb/experimental/ITask_TwoPartArmMC.java
@@ -4,6 +4,7 @@
  */
 package gjb.experimental;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
 import gjb.interfaces.LoggingInterface;
@@ -20,8 +21,8 @@ public class ITask_TwoPartArmMC implements TaskInterface {
 
     // Place additional instance variables here - like sub system objects..
     SubSysIntake intakeS;
-
-
+    boolean assistActive = false;
+    boolean encoderReset = false; //arm motor
 
     // Modify this constructor to add any additional initialization parameters - see
     // other tasks for examples.
@@ -55,6 +56,7 @@ public class ITask_TwoPartArmMC implements TaskInterface {
     public void start() {
         rt.resetStartTime();
         // Any code to execute ONCE just when the op mode is started goes here.
+        autonAssist_start();
     }
 
 
@@ -70,7 +72,8 @@ public class ITask_TwoPartArmMC implements TaskInterface {
         bintakeSliderLogic();
         biggulpLogic();
         arMadilloLogic();
-
+        autonAssist();
+        autonAssist_loop();
     }
 
 
@@ -133,66 +136,41 @@ public class ITask_TwoPartArmMC implements TaskInterface {
 
         rt.telemetry().addData("onInside", intakeS.onInside);
 
-        double power = intakeS.STRIDE_STOP;
 
-        if (intakeS.limit_mid_pressed()) {
-            power = intakeS.STRIDE_STOP;
+
+        /*if (intakeS.limit_mid_pressed()) {
+            intakeS.stridePower = intakeS.STRIDE_STOP;
             if (intakeS.bean_slider_out()) {
-                power = intakeS.STRIDE_OUT_POWER;
+                intakeS.stridePower = intakeS.STRIDE_OUT_POWER;
             }
         } else if (intakeS.bean_slider_in()) {
-            power = intakeS.STRIDE_IN_POWER;
-        }
+            intakeS.stridePower = intakeS.STRIDE_IN_POWER;
+        } */
+
+        if(!intakeS.bean_slider_out() && !intakeS.bean_slider_in())
+            intakeS.stridePower = intakeS.STRIDE_STOP;
 
         if (intakeS.limit_in_pressed()) {
-            power = intakeS.STRIDE_STOP;
+            intakeS.stridePower = intakeS.STRIDE_STOP;
             if (intakeS.bean_slider_out()) {
-                power = intakeS.STRIDE_OUT_POWER;
+                intakeS.stridePower = intakeS.STRIDE_OUT_POWER;
             }
         } else if (intakeS.bean_slider_in()) {
-            power = intakeS.STRIDE_IN_POWER;
+            intakeS.stridePower = intakeS.STRIDE_IN_POWER;
         }
 
         if (intakeS.limit_out_pressed()) {
             //Can't go out anymore
-            power = intakeS.STRIDE_STOP;
+            intakeS.stridePower = intakeS.STRIDE_STOP;
             if (intakeS.bean_slider_in()) {
-                power = intakeS.STRIDE_IN_POWER;
+                intakeS.stridePower = intakeS.STRIDE_IN_POWER;
             }
         } else if (intakeS.bean_slider_out()) {
-            power = intakeS.STRIDE_OUT_POWER;
-        }
-        // mira's auton assist code
-        if(intakeS.limit_mid_pressed()){
-            if(power == intakeS.STRIDE_IN_POWER)
-                intakeS.onInside = true;
-            if(power == intakeS.STRIDE_OUT_POWER)
-                intakeS.onInside = false;
+            intakeS.stridePower = intakeS.STRIDE_OUT_POWER;
         }
 
-        if(rt.gamepad2().b() ==  true || !intakeS.bFinished) {
-            intakeS.bFinished = false;
-            if (!intakeS.limit_mid_pressed()) {
-                rt.telemetry().addData("limit_switch_mid", "not pressed");
-                if (!intakeS.limit_switch_in.getState() || intakeS.onInside) {//Can't go in anymore/on the inside from middle
-                    intakeS.onInside = true;
-                    power = intakeS.STRIDE_OUT_POWER;
-                    rt.telemetry().addData("limit_switch_in", "pressed");
-                }
-                if (!intakeS.limit_switch_out.getState() || !intakeS.onInside) {//Can't go out anymore/not on inside from middle
-                    intakeS.onInside = false;
-                    power = intakeS.STRIDE_IN_POWER;
-                    rt.telemetry().addData("limit_switch_out", "pressed");
-                }
-            } else {
-                power = intakeS.STRIDE_STOP;
-                rt.telemetry().addData("limit_switch_mid", "pressed");
-                intakeS.bFinished = true;
-            }
-        }
-        // end of Mira's auton assist code
 
-        intakeS.strider.setPosition(power);
+        intakeS.strider.setPosition(intakeS.stridePower);
 
         //
         // ------------ Telemetry ---------------------------------
@@ -200,7 +178,7 @@ public class ITask_TwoPartArmMC implements TaskInterface {
 
         boolean pressed = intakeS.limit_mid_pressed();
         rt.telemetry().addData("limit_switch_mid pressed: ", pressed);
-        rt.telemetry().addData("stride power", power);
+        rt.telemetry().addData("stride power", intakeS.stridePower);
     }
 
     public void biggulpLogic(){
@@ -230,7 +208,9 @@ public class ITask_TwoPartArmMC implements TaskInterface {
         //high value indicated being pressed
         //we took out the inverted thing because we are using normal limit switches for the arm
 
-
+        if(assistActive ) {
+            return; //--------------------------- EARLY RETURN -------------------------------\\
+        }
         double power = 0;
         if (intakeS.limit_forward_pressed()) {
             //Can't go forward/downwards anymore
@@ -261,6 +241,143 @@ public class ITask_TwoPartArmMC implements TaskInterface {
         // Send telemetry message to signify robot running;
         // rt.telemetry().addData("claw",  "Offset = %.2f", clawOffset);
     }
-    // Place additional helper methods here.
+    // mira's auton assist code
+    public void autonAssist(){
+        if(intakeS.limit_mid_pressed()){
+            if(intakeS.stridePower == intakeS.STRIDE_IN_POWER) {
+                intakeS.onInside = true;
+            }
+            if(intakeS.stridePower == intakeS.STRIDE_OUT_POWER) {
+                intakeS.onInside = false;
+            }
+        }
 
+        //rt.telemetry().addData("assistContinue", intakeS.assistContinue);
+
+        /*if(rt.gamepad2().b()){
+            if(intakeS.assistContinue) {//to cancel
+                intakeS.assistContinue = false;
+            } else {
+                intakeS.assistContinue = true;
+            }
+        }*/
+
+        //if(intakeS.assistContinue) {
+        if(rt.gamepad2().b()){
+            if(intakeS.biggulp.getPosition() != intakeS.MAX_STICK)
+                intakeS.biggulp.setPosition(intakeS.MAX_STICK);
+            if (!intakeS.limit_out_pressed()) {
+                rt.telemetry().addData("limit_switch_out", "not pressed");
+                if(intakeS.strider.getPosition() != intakeS.STRIDE_OUT_POWER){
+                    intakeS.strider.setPosition(intakeS.STRIDE_OUT_POWER);
+                }
+
+                /*if (!intakeS.limit_switch_in.getState() || intakeS.onInside) {//Can't go in anymore/on the inside from middle
+                    intakeS.onInside = true;
+                    intakeS.strider.setPosition(intakeS.STRIDE_OUT_POWER);
+                    //intakeS.stridePower = intakeS.STRIDE_OUT_POWER;
+                    //rt.telemetry().addData("limit_switch_in", "pressed");
+                }else {
+                    intakeS.onInside = false;
+                    intakeS.strider.setPosition(intakeS.STRIDE_IN_POWER);
+                }*/
+
+                /*if (!intakeS.limit_switch_out.getState() || !intakeS.onInside) {//Can't go out anymore/not on inside from middle
+                    intakeS.onInside = false;
+                    intakeS.stridePower = intakeS.STRIDE_IN_POWER;
+                    rt.telemetry().addData("limit_switch_out", "pressed");
+                }*/
+            } else {
+                intakeS.stridePower = intakeS.STRIDE_STOP;
+                rt.telemetry().addData("limit_switch_mid", "pressed");
+                if(!intakeS.limit_backward_pressed()){
+                    intakeS.ArmaDillo.setPower(intakeS.rampedPower(intakeS.ArmaDillo, intakeS.DILLO_BKWD-.05));
+                }else{
+                    intakeS.ArmaDillo.setPower(intakeS.rampedPower(intakeS.ArmaDillo, 0));
+                    //intakeS.assistContinue = false;
+                }
+            }
+            //intakeS.strider.setPosition(intakeS.stridePower);
+
+        }
+
+        // end of Mira's auton assist code
+        //start of Keya's
+    }
+    public void autonAssist_loop(){
+        //as soon as it hits the backward limit switch, if the current encoder value is not 0, set it to 0
+        if(intakeS.limit_backward_pressed()){
+            int ticks = intakeS.ArmaDillo.getCurrentPosition();
+            if(assistActive){
+                autonAssist_end();
+            } else if(ticks<10){
+                intakeS.ArmaDillo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                encoderReset = true;
+                log("resetting encoder");
+            }
+        }
+
+        if(beanActive()){
+            autonAssist_cancel();
+        } else if (rt.gamepad2().x()&& !assistActive){
+            assistActive = true;
+            log("Assist is starting");
+            if(!encoderReset){
+                log("encoders weren't reset!!");
+                autonAssist_cancel();
+            } else {
+                intakeS.ArmaDillo.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                intakeS.biggulp.setPosition(intakeS.GULP_START);
+                intakeS.ArmaDillo.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                intakeS.ArmaDillo.setTargetPosition(0);
+                intakeS.ArmaDillo.setPower(intakeS.DILLO_BKWD);
+            }
+        }
+    }
+
+    public void autonAssist_cancel(){
+        if(assistActive) {
+            intakeS.ArmaDillo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            intakeS.ArmaDillo.setPower(0);
+            log("assist was canceled");
+            assistActive = false;
+        }
+    }
+
+    public void autonAssist_start(){
+        //set power to be negative (safely goes back)
+        if(!intakeS.limit_backward_pressed()) {
+            intakeS.ArmaDillo.setPower(intakeS.DILLO_BKWD);
+            log("set arm power to negative");
+        }
+    }
+
+    public void autonAssist_end(){
+        if(assistActive) {
+            intakeS.ArmaDillo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            intakeS.ArmaDillo.setPower(0);
+            log("assist has finished");
+            assistActive = false;
+        }
+    }
+    //if anything on the 2nd controller or the dpad on the first controller is pressed, then it returns true, because a driver is pressing something
+    public boolean beanActive(){
+        return intakeS.bean_slider_out() || intakeS.bean_ArmaDillo_backward() || intakeS.bean_ArmaDillo_forward() || intakeS.bean_slider_in()
+                || rt.gamepad1().dpad_up() || rt.gamepad1().dpad_down();
+    }
+    /* planning:
+    when start is pressed, Start method gets called
+        set power to be negative (safely goes back)
+        loop: as soon as it hits the backward limit switch, if the current encoder value is not 0, set it to 0
+        create a method: if anything on the 2nd controller or the dpad on the first controller is pressed, then it returns false
+        it will disable auton assist if the method returns true.
+        If the bumper is pressed, enable auton assist, and set motor to RUN_USING_ENCODERS
+        startAssist: set biggulp to 0.9, setPosition to 0
+     */
+    // end of keya's auton assist code
+
+    // Place additional helper methods here.
+    void log(String s) {
+        rt.telemetry().log().add("Arm: " + s);
+    }
 }
